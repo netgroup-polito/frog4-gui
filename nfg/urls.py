cabb661892b505de1c211cc56f7be6e3735d26e5
giacomo <giacomo.ranieri@studenti.polito.it>
2016-04-27 21:00:06 +0200

    residuo merge

--- nfg/urls.py
+++ nfg/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [url(r'^$', views.index, name='index'),
                url(r'^deploy/$', views.deploy, name='deploy'),
                url(r'^users/$', views.users, name='users'),
                url(r'^api/v1/users_api/get_user_list/$', views.api_get_user_list, name='get_user_list'),
+               url(r'^api/v1/users_api/get_group_list/$', views.api_get_group_list, name='get_group_list')
 
                ]
