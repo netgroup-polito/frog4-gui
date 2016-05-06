41e82554d185eb1f390d706c88a563440699308d
valentinpopov <popovvalentino@hotmail.it>
2016-05-06 14:36:53 +0200

    retreive templates VNF

--- nfg/template_manager.py
+++ nfg/template_manager.py
@@ -1,36 +1,24 @@
-/**
- * Created by valentin on 03/05/16.
- */
+
 import json
 import requests
 
-
-class NFFGManager:
+class TemplateManager:
     def __init__(self, host, port):
         self.un_protocol = 'http'
         self.un_host = host
         self.un_port = port
         self.base_path = ''
 
-    def get_user_graph(self, graphId, token):
-        headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
+    def get_template(self, templateId):
+        headers = {'Content-type': 'application/json'}
         response = requests.get(
-            self.un_protocol + '://' + self.un_host + ':' + self.un_port + '/' + self.base_path + 'NF-FG/' + graphId,
+            self.un_protocol + '://' + self.un_host + ':' + self.un_port + '/' + self.base_path + 'v1/VNF/' + templateId+'/?format=json',
             headers=headers)
         if response.status_code == 200:
+            t=json.loads(response.content)["list"]
             return {"status": response.status_code,
-                    "forwarding-graph": json.loads(response.content)["forwarding-graph"]}
+                    "template": json.loads(response.content)["list"]}
         else:  # todo: gestione errori comuni
             return {"status": response.status_code, "error": "Unknown Error"}
 
-    def add_user_graph(self, graphId, nffg, token):
-        data_json = json.dumps(nffg)
-        headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
-        response = requests.put(
-            self.un_protocol + '://' + self.un_host + ':' + self.un_port + '/' + self.base_path + 'NF-FG/' + graphId,
-            data=data_json,
-            headers=headers)
-        if response.status_code == 201:
-            return {"status": response.status_code}
-        else:  # todo: gestione errori comuni
-            return {"status": response.status_code, "error": "Unknown Error"}
+
