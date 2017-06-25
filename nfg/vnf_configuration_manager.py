8b3ce48f4ddb36b7fcebd116a0d08ce6f0c78206
piscoroma <piscoroma@gmail.com>
2017-06-25 22:03:49 +0200

    updated url to contact the configuration-orchestrator

--- nfg/vnf_configuration_manager.py
+++ nfg/vnf_configuration_manager.py
@@ -3,33 +3,27 @@ import requests
 
 
 class VNFConfigurationManager:
-    def __init__(self, host, port):
-        self.un_protocol = 'http'
-        self.un_host = host
-        self.un_port = port
-        self.base_path = ''
+    def __init__(self, config_orch_endpoint):
+        self.base_url = config_orch_endpoint
 
-    def get_vnf_state(self, tenant_id, graph_id, vnf_identifier):
+    def get_vnf_state(self, tenant_id, graph_id, vnf_id):
         # here a control on the input parameter should be done
         headers = {'Content-type': 'application/json'}
-        path = "config/status/" + vnf_identifier + '/' + graph_id + '/' + tenant_id + '/'
-        response = requests.get(
-            self.un_protocol + '://' + self.un_host + ':' + self.un_port + '/' + self.base_path + path, headers=headers)
+        get_status_url = self.base_url + '/' + tenant_id + '/' + graph_id + '/' + vnf_id + '/'
+        response = requests.get(get_status_url, headers=headers)
         if response.status_code == 200:
             # t=json.loads(response.content)["list"]
             return {"status": response.status_code,
                     "state": json.loads(response.content)}
         else:  # todo: gestione errori comuni
-            return {"status": response.status_code, "error": "Unknown Error"}
+            return {"status": response.status_code, "error": json.loads(response.content)}
 
-    def put_vnf_updated_state(self, tenant_id, graph_id, vnf_identifier, data, token):
+    def put_vnf_updated_state(self, tenant_id, graph_id, vnf_id, data, token):
         headers = {'Content-type': 'application/json', 'X-Auth-Token': token}
-        path = "config/vnf/" + vnf_identifier + '/' + graph_id + '/' + tenant_id + '/'
-        response = requests.put(
-            self.un_protocol + '://' + self.un_host + ':' + self.un_port + '/' + self.base_path + path, data=data,
-            headers=headers)
+        push_status_url = self.base_url + '/' + tenant_id + '/' + graph_id + '/' + vnf_id + '/'
+        response = requests.put(push_status_url, data=data, headers=headers)
         print(response)
         if response.status_code == 200:
             return {"status": response.status_code}
         else:  # todo: gestione errori comuni
-            return {"status": response.status_code, "error": "Unknown Error"}
+            return {"status": response.status_code, "error": json.loads(response.content)}
