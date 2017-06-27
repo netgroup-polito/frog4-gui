13ad55300132e52d2b66f78f4cba4ba53c7bd07e
DavideXX92 <davithepezz@hotmail.it>
2017-06-27 14:50:14 +0200

    Configuration modal -> added support for leafref directive

--- nfg/static/js/fg-d3/directives/leafDirective.js
+++ nfg/static/js/fg-d3/directives/leafDirective.js
@@ -6,6 +6,7 @@
         return {
             restrict: "E",
             scope: {
+                rootObject: "=",
                 leafObject: "=",
                 stateObject: "="
             },
